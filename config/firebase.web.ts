191ce564dfcd416b3fc6e6a7136cbf06dc51c413
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-08-07 12:55:46 +0900

    envの更新

--- config/firebase.web.ts
+++ config/firebase.web.ts
@@ -1,9 +1,9 @@
 export const config = {
-  apiKey: "AIzaSyBLwPlQ830gWHITjZHlMknpTDLSSUKIX14",
-  authDomain: "fir-clerk.firebaseapp.com",
-  projectId: "fir-clerk",
-  storageBucket: "fir-clerk.appspot.com",
-  messagingSenderId: "143013331542",
-  appId: "1:143013331542:web:2d1da19ba86cce5fce71f7",
-  measurementId: "G-1EL2GSVF49",
+  apiKey: "AIzaSyCAq_Qbq8vf886KfVLCu8Pong_r4WeWP_U",
+  authDomain: "clerk-projects.firebaseapp.com",
+  projectId: "clerk-projects",
+  storageBucket: "clerk-projects.appspot.com",
+  messagingSenderId: "809334862220",
+  appId: "1:809334862220:web:523680464739c481ae26a9",
+  // measurementId: "G-1EL2GSVF49",
 };
